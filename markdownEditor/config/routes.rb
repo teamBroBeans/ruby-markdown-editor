0abd980e47fe2dc8cc1b6bbdb203703669f5a0d2
Sam Bristol <samuel.d.bristol@gmail.com>
2015-09-17 20:58:40 +0000

    Modify route to use /editor/index

--- markdownEditor/config/routes.rb
+++ markdownEditor/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index' => 'editor#index'
+  get '/editor/index' => 'editor#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
