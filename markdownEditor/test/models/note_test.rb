f5e90eee8ceacd69ad384b3065d6e14d224e2e6d
ireineck <ireineck@student.cvtc.edu>
2015-09-19 12:30:45 -0500

    adds unit test that confirms that a note can be saved

--- markdownEditor/test/models/note_test.rb
+++ markdownEditor/test/models/note_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creates valid record" do
+    note = Note.new
+    note.title = "Hello World"
+    note.tag = "#hiWorld"
+    note.inputText = "#Hello World# "
+    assert note.save
+end
+  
 end
