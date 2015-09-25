a2a19b80af8cc3945ee9ccbbcfff74eb689fc167
Sam Bristol <samuel.d.bristol@gmail.com>
2015-09-25 16:55:04 +0000

    Modify schema.rb to support note_tags table

--- markdownEditor/db/schema.rb
+++ markdownEditor/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924201805) do
+ActiveRecord::Schema.define(version: 20150925165043) do
+
+  create_table "note_tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
