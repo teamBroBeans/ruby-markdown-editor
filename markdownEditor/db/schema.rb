f83e83ea1a4db6c59f96422d0ab3242fc59e6f66
Sam Bristol <samuel.d.bristol@gmail.com>
2015-09-25 17:23:24 +0000

    Update schema to support note_tag foreign key to notes

--- markdownEditor/db/schema.rb
+++ markdownEditor/db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925165043) do
+ActiveRecord::Schema.define(version: 20150925171511) do
 
   create_table "note_tags", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "note_id"
   end
+
+  add_index "note_tags", ["note_id"], name: "index_note_tags_on_note_id"
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
