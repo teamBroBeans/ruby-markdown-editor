e65c02aedbc2a5e0797dfbe5a54bee515e0a37ba
ireineck <ireineck@student.cvtc.edu>
2015-09-24 20:57:03 -0500

    adds test to find 2 matches on title

--- markdownEditor/test/models/note_test.rb
+++ markdownEditor/test/models/note_test.rb
@@ -29,19 +29,29 @@ class NoteTest < ActiveSupport::TestCase
 
   test "creates an invalid record- title > 250 characters" do
     note = Note.new
-    note.title = "this is klingon: Cha'dich lolmoh mevyap nahnagh qaryoq qul tuq qid tijwi'ghom ting 'arlogh 'e'mam 'iqnah. Chegh chuq'a' ghet ghojmeh taj jer lol marwi' ngop pa' red piq qaryoq'a' qawhaq quy'ip qap raw' sosbor'a' tagh tlhach mu'mey va vem'eq waqboch yatqap. Bo denibngan loltah qirq sosbor tiqnagh lemdu' tlhup vad yer 'orghengan. Bop chuq'a' dugh ghitlhwi' magh notqa' pur red tagh tahqeq tlhup. Divi' homwi' lev logh marwi' naw' pa' beb qa'rol qeylis qirq roswi' tennus tlhach mu'mey yiq yuqjijqa' 'edjen 'iqnah 'orghenya'ngan. Be'joy' boq'egh chang'eng cha'qu' chen chuy denib qatlh denibya' der ghitlhwi' jer lev lolchu' nan naq nga'chuq qa'meh rav'eq ron segh senwi' rilwi' je tahqeq tey' torgh wovmohwi' yiq yuqjijdivi' 'ud'a'.
-        
-"
+    note.title = "this is klingon: Cha'dich lolmoh mevyap nahnagh qaryoq qul tuq qid tijwi'ghom ting 'arlogh 'e'mam 'iqnah. Chegh chuq'a' ghet ghojmeh taj jer lol marwi' ngop pa' red piq qaryoq'a' qawhaq quy'ip qap raw' sosbor'a' tagh tlhach mu'mey va vem'eq waqboch yatqap. Bo denibngan loltah qirq sosbor tiqnagh lemdu' tlhup vad yer 'orghengan. Bop chuq'a' dugh ghitlhwi' magh notqa' pur red tagh tahqeq tlhup. Divi' homwi' lev logh marwi' naw' pa' beb qa'rol qeylis qirq roswi' tennus tlhach mu'mey yiq yuqjijqa' 'edjen 'iqnah 'orghenya'ngan. Be'joy' boq'egh chang'eng cha'qu' chen chuy denib qatlh denibya' der ghitlhwi' jer lev lolchu' nan naq nga'chuq qa'meh rav'eq ron segh senwi' rilwi' je tahqeq tey' torgh wovmohwi' yiq yuqjijdivi' 'ud'a'."
         note.tag = "#klignon"
         note.inputText = "#Hello World# "
         assert !note.save
   end
 
-test "should find a match on title" do
+  test "should find a match on title" do
     test_note = Note.create(title: "Klingon",
                                 tag: "#WorfWasAwesome",
                                 inputText: "this is klingon: Cha'dich lolmoh mevyap nahnagh qaryoq qul tuq qid tijwi'ghom ting 'arlogh 'e'mam 'iqnah. Chegh chuq'a' ghet ghojmeh taj jer lol marwi' ngop pa' red piq qaryoq'a' qawhaq quy'ip qap raw' sosbor'a' tagh tlhach mu'mey va vem'eq waqboch yatqap. Bo denibngan loltah qirq sosbor tiqnagh lemdu' tlhup vad yer 'orghengan. Bop chuq'a' dugh ghitlhwi' magh notqa' pur red tagh tahqeq tlhup. Divi' homwi' lev logh marwi' naw' pa' beb qa'rol qeylis qirq roswi' tennus tlhach mu'mey yiq yuqjijqa' 'edjen 'iqnah 'orghenya'ngan. Be'joy' boq'egh chang'eng cha'qu' chen chuy denib qatlh denibya' der ghitlhwi' jer lev lolchu' nan naq nga'chuq qa'meh rav'eq ron segh senwi' rilwi' je tahqeq tey' torgh wovmohwi' yiq yuqjijdivi' 'ud'a'.")
     assert_equal test_note, Note.find_all_by_query("klingon").first
-end
+    
+    end
+
+  test "should find 2 matches on title" do
+    Note.create!(title: "Klingon",
+                 tag: "#todayIsAGoodDayToDie",
+                 inputText: "Cha'dich lolmoh mevyap nahnagh qaryoq qul tuq qid tijwi'ghom ting 'arlogh 'e'mam 'iqnah. Chegh chuq'a' ghet ghojmeh taj jer lol marwi' ngop pa' red piq qaryoq'a' qawhaq quy'ip qap raw' sosbor'a' tagh tlhach mu'mey va vem'eq waqboch yatqap. Bo denibngan loltah qirq sosbor tiqnagh lemdu' tlhup vad yer 'orghengan. Bop chuq'a' dugh ghitlhwi' magh notqa' pur red tagh tahqeq tlhup. Divi' homwi' lev logh marwi' naw' pa' beb qa'rol qeylis qirq roswi' tennus tlhach mu'mey yiq yuqjijqa' 'edjen 'iqnah 'orghenya'ngan. Be'joy' boq'egh chang'eng cha'qu' chen chuy denib qatlh denibya' der ghitlhwi' jer lev lolchu' nan naq nga'chuq qa'meh rav'eq ron segh senwi' rilwi' je tahqeq tey' torgh wovmohwi' yiq yuqjijdivi' 'ud'a'.")
+                 
+    Note.create!(title: "theKlingonEmpire",
+                   tag: "#todayIsAGoodDayToDie",
+                   inputText: "Cha'dich lolmoh mevyap nahnagh qaryoq qul tuq qid tijwi'ghom ting 'arlogh 'e'mam 'iqnah. Chegh chuq'a' ghet ghojmeh taj jer lol marwi' ngop pa' red piq qaryoq'a' qawhaq quy'ip qap raw' sosbor'a' tagh tlhach mu'mey va vem'eq waqboch yatqap. Bo denibngan loltah qirq sosbor tiqnagh lemdu' tlhup vad yer 'orghengan. Bop chuq'a' dugh ghitlhwi' magh notqa' pur red tagh tahqeq tlhup. Divi' homwi' lev logh marwi' naw' pa' beb qa'rol qeylis qirq roswi' tennus tlhach mu'mey yiq yuqjijqa' 'edjen 'iqnah 'orghenya'ngan. Be'joy' boq'egh chang'eng cha'qu' chen chuy denib qatlh denibya' der ghitlhwi' jer lev lolchu' nan naq nga'chuq qa'meh rav'eq ron segh senwi' rilwi' je tahqeq tey' torgh wovmohwi' yiq yuqjijdivi' 'ud'a'....")
+    assert_equal 2, Note.find_all_by_query("klingon").count
+  end
 
 end
