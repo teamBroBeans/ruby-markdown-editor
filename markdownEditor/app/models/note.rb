63b5d2dc0bc6347afa8e4e76f3ab6b236193c3e1
ireineck <ireineck@student.cvtc.edu>
2015-09-24 20:33:52 -0500

    adds method to note.rb to search by title

--- markdownEditor/app/models/note.rb
+++ markdownEditor/app/models/note.rb
@@ -1,6 +1,9 @@
 class Note < ActiveRecord::Base
     validates_presence_of :title, :tag, :inputText
     validates :title, length: { maximum: 250,too_long: "%{count} characters is the maximum allowed"}
-
+    
+    def self.find_all_by_query(query) query = query.downcase
+    query = "%#{query}%"
+    Note.where(['lower(title) like ?', query]) end
 
 end
