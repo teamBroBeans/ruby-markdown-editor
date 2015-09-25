e6163a264d5ee075f896f39a9887c4a742865ada
ireineck <ireineck@student.cvtc.edu>
2015-09-24 21:02:49 -0500

    fixes a spacing issue in code note_test.rb

--- markdownEditor/test/models/note_test.rb
+++ markdownEditor/test/models/note_test.rb
@@ -40,8 +40,7 @@ class NoteTest < ActiveSupport::TestCase
                                 tag: "#WorfWasAwesome",
                                 inputText: "this is klingon: Cha'dich lolmoh mevyap nahnagh qaryoq qul tuq qid tijwi'ghom ting 'arlogh 'e'mam 'iqnah. Chegh chuq'a' ghet ghojmeh taj jer lol marwi' ngop pa' red piq qaryoq'a' qawhaq quy'ip qap raw' sosbor'a' tagh tlhach mu'mey va vem'eq waqboch yatqap. Bo denibngan loltah qirq sosbor tiqnagh lemdu' tlhup vad yer 'orghengan. Bop chuq'a' dugh ghitlhwi' magh notqa' pur red tagh tahqeq tlhup. Divi' homwi' lev logh marwi' naw' pa' beb qa'rol qeylis qirq roswi' tennus tlhach mu'mey yiq yuqjijqa' 'edjen 'iqnah 'orghenya'ngan. Be'joy' boq'egh chang'eng cha'qu' chen chuy denib qatlh denibya' der ghitlhwi' jer lev lolchu' nan naq nga'chuq qa'meh rav'eq ron segh senwi' rilwi' je tahqeq tey' torgh wovmohwi' yiq yuqjijdivi' 'ud'a'.")
     assert_equal test_note, Note.find_all_by_query("klingon").first
-    
-    end
+  end
 
   test "should find 2 matches on title" do
     Note.create!(title: "Klingon",
