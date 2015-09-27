85ed0741359d5fd567b1203547b78b84f7c5e1b8
Sam Bristol <samuel.d.bristol@gmail.com>
2015-09-27 01:59:37 +0000

    Add relationship to note_tags and notes

--- markdownEditor/app/models/note.rb
+++ markdownEditor/app/models/note.rb
@@ -1,6 +1,7 @@
 class Note < ActiveRecord::Base
     validates_presence_of :title, :tag, :inputText
     validates :title, length: { maximum: 250,too_long: "%{count} characters is the maximum allowed"}
-
+    has_many :note_tags
+    has_many :tags, :through => :note_tags
 
 end
