053e3bd8c882cf049da5c00708c08a663a8d9a4c
Sam Bristol <samuel.d.bristol@gmail.com>
2015-09-28 15:24:20 +0000

    Add set all tags method to enter tags into tag table from :tag, add get all tags method to get a comma separated string of tagnames

--- markdownEditor/app/models/note.rb
+++ markdownEditor/app/models/note.rb
@@ -4,4 +4,15 @@ class Note < ActiveRecord::Base
     has_many :note_tags
     has_many :tags, :through => :note_tags
 
+    # Create tags from :tag
+    def set_all_tags
+        self.tags = tag.split(",").map do |name|
+            Tag.where(name: name.strip).first_or_create!
+        end
+    end
+    
+    # Get :tag string from tags
+    def get_all_tags
+        self.tags.map(&:name).join(", ")
+    end
 end
