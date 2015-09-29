821eaec07f5a7ce278e8a439adfa325fecbe72e3
Sam Bristol <samuel.d.bristol@gmail.com>
2015-09-29 02:09:56 +0000

    Add test and assertion that NoteTag cannot be saved without note_id

--- markdownEditor/test/models/notes_tag_test.rb
+++ markdownEditor/test/models/notes_tag_test.rb
@@ -11,8 +11,17 @@ class NotesTagTest < ActiveSupport::TestCase
   
   test "should not save if tag_id missing" do
     noteTag = NotesTag.new
+    noteTag.note_id = 1
     
     assert !noteTag.save
     assert noteTag.errors[:tag_id].include?("can't be blank")
+  end
+  
+  test "should not save if note_id missing" do
+    noteTag = NotesTag.new
+    noteTag.tag_id = 1
+    
+    assert !noteTag.save
+    assert noteTag.errors[:note_id].include?("can't be blank")
   end
 end
