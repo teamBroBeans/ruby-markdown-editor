b0e37de6356d13ad10f44f5c7134b308a93cd56a
Sam Bristol <samuel.d.bristol@gmail.com>
2015-09-29 01:41:48 +0000

    Add test to create a valid record

--- markdownEditor/test/models/note_tag_test.rb
+++ markdownEditor/test/models/note_tag_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class NoteTagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creates valid record" do
+    tag = Tag.new
+    tag.name = "Test Tag"
+    assert tag.save
+  end
 end
