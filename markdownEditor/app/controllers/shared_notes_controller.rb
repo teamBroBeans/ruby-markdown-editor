c0e51bfe9b729507a883631d9ac306d59b7c24ce
ireineck <ireineck@student.cvtc.edu>
2015-10-03 18:37:16 -0500

    adds method for showing a shared note

--- markdownEditor/app/controllers/shared_notes_controller.rb
+++ markdownEditor/app/controllers/shared_notes_controller.rb
@@ -1,4 +1,9 @@
 class SharedNotesController < ApplicationController
   def show
+      @note = Note.find_by_slug @note
+      if @note
+        render :show
+      else
+        redirect_to root_url, notice: "That note does not exist"
   end
 end
