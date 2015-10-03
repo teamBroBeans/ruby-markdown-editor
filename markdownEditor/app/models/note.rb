42246aca6c5b6a3762f849f9a0cca687bd89a8f8
ireineck <ireineck@student.cvtc.edu>
2015-10-03 12:51:46 -0500

    adds require securerandom to note.rb

--- markdownEditor/app/models/note.rb
+++ markdownEditor/app/models/note.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 class Note < ActiveRecord::Base
     validates_presence_of :title, :tag, :inputText
     validates :title, length: { maximum: 250,too_long: "%{count} characters is the maximum allowed"}
