370a433350c49590989f19aa439bdb980ee46334
csiess <csiess@student.cvtc.edu>
2015-10-05 02:47:58 +0000

    Stule the preview window

--- markdownEditor/config/routes.rb
+++ markdownEditor/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :notes
   get "home/textEditor"
-  root controller: 'home', action: 'textEditor'
+  root controller: 'notes', action: 'index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
