4aa82c70d2e0cc703c1dba7b93add9c743f1bb06
csiess <csiess@student.cvtc.edu>
2015-10-12 13:52:32 +0000

    Add feature for the user to only view his own notes in the trashcan

--- markdownEditor/app/controllers/trashcan_controller.rb
+++ markdownEditor/app/controllers/trashcan_controller.rb
@@ -1,12 +1,14 @@
 class TrashcanController < ApplicationController
   def index
     @notes = Note.where(:inTrashcan => true)
+    @notes = Note.where('user_id = ? AND
+                            inTrashcan = ?', current_user, true)
   end
 
   def create
     @note = Note.find(params[:id])
     @note.inTrashcan = true
-    
+    @note.user = current_user
     @note.save
     
     redirect_to notes_url
